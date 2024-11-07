c92d020532f04b87a1cc84f5698cd11b707dc1f9
mulan04 <22234264+mulan04@users.noreply.github.com>
2024-11-07 17:38:20 +0100

    Update insecure-db.tf

--- insecure-db.tf
+++ insecure-db.tf
@@ -3,7 +3,6 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  name                 = "mydb"
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
