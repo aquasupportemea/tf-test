34e9d2514ff69d0afe8e428121da6161d0a41fd1
Andreas Müller <andreas.muller@aquasec.com>
2024-11-07 18:51:32 +0100

    Signed-off-by: Andreas Müller <andreas.muller@aquasec.com>

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,6 @@ output "cluster_security_group_id" {
   value = module.eks.cluster_security_group_id
 }
 
-output "node_group_role_arn" {
-  value = module.eks.node_groups["eks_nodes"].iam_role_arn
-}
+# output "node_group_role_arn" {
+  # value = module.eks.node_groups["eks_nodes"].iam_role_arn
+# }
