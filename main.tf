2465c49bc26dd051b511af7b5d751bef12334737
Andreas Müller <andreas.muller@aquasec.com>
2024-11-07 18:49:10 +0100

    Signed-off-by: Andreas Müller <andreas.muller@aquasec.com>

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "4.0.0"
+  # version = "4.0.0"
 
   name                 = "${var.cluster_name}-vpc"
   cidr                 = var.vpc_cidr
   azs                  = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  # enable_nat_gateway   = true
+  # single_nat_gateway   = true
   public_subnet_tags   = { "kubernetes.io/role/elb" = "1" }
   private_subnet_tags  = { "kubernetes.io/role/internal-elb" = "1" }
 }
@@ -21,10 +21,10 @@ module "vpc" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "18.0.0"
+  # version         = "18.0.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.21"
+  # cluster_version = "1.21"
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
   vpc_id          = module.vpc.vpc_id
