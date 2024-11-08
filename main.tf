0d5b4bc2756b199d1b9ce9038de729b6e605b594
Andreas Müller <andreas.muller@aquasec.com>
2024-11-08 09:08:39 +0100

    Signed-off-by: Andreas Müller <andreas.muller@aquasec.com>

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   access_key    = var.aws_access_key
   secret_key    = var.aws_secret_key
-  session_token = var.aws_session_token
+  # session_token = var.aws_session_token
   region = var.region
 }
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "aws_session_token" {}
+# variable "aws_session_token" {}
 
 
 
