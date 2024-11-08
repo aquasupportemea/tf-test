8d14f3f1359ddc9cdf65fc7d5e281b6e40be63fa
Andreas Müller <andreas.muller@aquasec.com>
2024-11-08 11:05:27 +0100

    Signed-off-by: Andreas Müller <andreas.muller@aquasec.com>

--- main.tf
+++ main.tf
@@ -1,15 +1,6 @@
 provider "aws" {
-  # access_key    = var.aws_access_key
-  # secret_key    = var.aws_secret_key
-  # session_token = var.aws_session_token
   region = var.region
 }
-
-# variable "aws_access_key" {}
-# variable "aws_secret_key" {}
-# variable "aws_session_token" {}
-
-
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
